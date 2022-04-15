c323918bde69611529bd5801ee171ec46d7077bf
Amar Gill <amardeep@clearbanc.com>
2022-04-15 16:08:32 -0400

    fix type errors

--- components/Crystal.tsx
+++ components/Crystal.tsx
@@ -1,10 +1,11 @@
 import { useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
+import { Mesh } from 'three';
 
 export default function Crystal() {
-  const mesh = useRef();
+  const mesh = useRef<Mesh>(null!);
 
-  useFrame((state, delta) => (mesh.current.rotation.y += 0.01));
+  useFrame((_state, _delta) => (mesh.current.rotation.y += 0.01));
   return (
     <mesh ref={mesh}>
       <icosahedronGeometry args={[2]} />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 
+import { AppProps } from 'next/app';
+import React from 'react';
+
 import AppHeader from '../components/AppHeader';
 
-function Layout({ children }) {
+function Layout({ children }: { children: React.ReactNode }) {
   return <div className='max-w-3xl mx-auto'>{children}</div>;
 }
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <AppHeader />
