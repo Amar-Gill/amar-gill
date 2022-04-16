418cc7b0fccbe28e5ef3a3b3bf6d17a3c24234de
Amar Gill <amardeep@clearbanc.com>
2022-04-16 12:28:11 -0400

    rename Layout to MainLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,17 @@ import React from 'react';
 
 import AppHeader from '../components/AppHeader';
 
-function Layout({ children }: { children: React.ReactNode }) {
-  return <div className='max-w-3xl mx-auto'>{children}</div>;
+function MainLayout({ children }: { children: React.ReactNode }) {
+  return <main className='max-w-3xl mx-auto'>{children}</main>;
 }
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <AppHeader />
-      <Layout>
+      <MainLayout>
         <Component {...pageProps} />
-      </Layout>
+      </MainLayout>
     </>
   );
 }
