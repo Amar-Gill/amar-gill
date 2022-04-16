a87ee7980d019f6f32458a75181368d43b2df972
Amar Gill <amardeep@clearbanc.com>
2022-04-16 12:17:05 -0400

    update to latest r3f version. remove ts-ignore for type error

--- components/Crystal.tsx
+++ components/Crystal.tsx
@@ -8,8 +8,6 @@ export default function Crystal() {
   useFrame((_state, _delta) => (mesh.current.rotation.y += 0.01));
 
   return (
-    // @ts-ignore
-    // TODO - fix build error
     <mesh ref={mesh}>
       <icosahedronGeometry args={[2]} />
       <meshStandardMaterial color='#9bc2c4' />
