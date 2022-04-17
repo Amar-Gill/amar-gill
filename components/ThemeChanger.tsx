2761b01cf3c47df8b01d944f112932ca1c80e73e
Amar Gill <amardeep@clearbanc.com>
2022-04-16 20:53:46 -0400

    basic themechanger component. set up theming on _app. configure global styles for themes

---
+++ components/ThemeChanger.tsx
@@ -0,0 +1,27 @@
+import { useTheme } from 'next-themes';
+import { useState, useEffect } from 'react';
+
+export default function ThemeChanger() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  function changeTheme() {
+    if (theme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  }
+
+  if (!mounted) return null;
+
+  return (
+    <div>
+      The current theme is: {theme}
+      <button onClick={changeTheme}>CHANGE THEME</button>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,31 @@ import { AppProps } from 'next/app';
 import React from 'react';
 
 import AppHeader from '../components/AppHeader';
+import { ThemeProvider } from 'next-themes';
+import ThemeChanger from '../components/ThemeChanger';
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   return <main className=' sm:p-0 max-w-3xl px-5 mx-auto'>{children}</main>;
 }
 
+function AppNav() {
+  return (
+    <nav>
+      <ThemeChanger />
+    </nav>
+  );
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <AppHeader />
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <ThemeProvider attribute='class'>
+        <AppNav />
+        <AppHeader />
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ThemeProvider>
     </>
   );
 }
